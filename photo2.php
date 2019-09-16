0a137e99765a935e9f35d8bf2ade71e8c7f2fe78
Desai1419 <53499783+Desai1419@users.noreply.github.com>
2019-09-16 09:57:11 +0800

    Create photo2.php

---
+++ photo2.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<head>Photo 2</head>
+<body bgcolor="black">
+  <main>
+  <img src="http://studioghiblimovies.com/images/laputaww7.jpg" width="800" height="600">
+  <a href="index.php">Back to home</a> <a href="photo3.php">Next Photo</a>
+    </body>    
+</html>
