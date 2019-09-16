666bed0850e937d828689c94c79194f169101328
Desai1419 <53499783+Desai1419@users.noreply.github.com>
2019-09-16 09:53:07 +0800

    Update images1.php

--- images1.php
+++ images1.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Assignment</title>
+  body {background-color: powderblue;}
+h1   {color: blue;}
+h2    {color: red;}
   </head>
 <body>
-  <h1>Rivas Photo Gallery</h1>
-  <h2>List of photos</h2>
+  <h1 align='Center'>Rivas Photo Gallery</h1>
+  <h2 align='Center'>List of photos</h2>
   <nav>
     <ul>
-      <li><a href="photo1.php">Photo 1</a></li>
-      <li><a href="photo2.php">Photo 2</a></li>
-      <li><a href="photo3.php">Photo 3</a></li>
+      <li align='Center'><a href="photo1.php">Photo 1</a></li>
+      <li align='Center'><a href="photo2.php">Photo 2</a></li>
+      <li align='Center'><a href="photo3.php">Photo 3</a></li>
     </ul>
   </nav>
   </body>
