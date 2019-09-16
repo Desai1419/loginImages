d43c3bd55c1d7f4b914caecf46d07526c6887898
Desai1419 <53499783+Desai1419@users.noreply.github.com>
2019-09-16 09:57:33 +0800

    Create photo3.php

---
+++ photo3.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<head>Photo 3</head>
+<body bgcolor="black">
+  <img src="https://images.alphacoders.com/736/thumb-1920-736461.png" width="800" height="600">
+  <a href="index.php">Back to home</a>
+</body>
+</html>
