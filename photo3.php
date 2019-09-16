dc6042f30f9d9861d5d0f3e89458d1193c469925
Desai1419 <53499783+Desai1419@users.noreply.github.com>
2019-09-16 09:59:09 +0800

    Update photo3.php

--- photo3.php
+++ photo3.php
@@ -4,6 +4,6 @@
 <head>Photo 3</head>
 <body bgcolor="black">
   <img src="https://images.alphacoders.com/736/thumb-1920-736461.png" width="800" height="600">
-  <a href="index.php">Back to home</a>
+  <a href="images1.php">Back to home</a>
 </body>
 </html>
