5a14d5fed44fa6aca8b1b332aa1ab1993d42175d
Desai1419 <53499783+Desai1419@users.noreply.github.com>
2019-09-16 09:58:41 +0800

    Update photo1.php

--- photo1.php
+++ photo1.php
@@ -4,6 +4,6 @@
 <head>Photo 1</head>
 <body bgcolor="black">
   <img src="https://wallpaperplay.com/walls/full/f/a/a/319224.jpg" width="800" height="600">
-  <a href="index.php">Back to home</a> <a href="photo2.php">Next Photo</a>
+  <a href="images1.php">Back to home</a> <a href="photo2.php">Next Photo</a>
 </body>
 </html>
