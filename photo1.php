f6d4f0fb2723896a831a3c85acdff3911530d13d
Desai1419 <53499783+Desai1419@users.noreply.github.com>
2019-09-16 09:56:50 +0800

    Create photo1.php

---
+++ photo1.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<head>Photo 1</head>
+<body bgcolor="black">
+  <img src="https://wallpaperplay.com/walls/full/f/a/a/319224.jpg" width="800" height="600">
+  <a href="index.php">Back to home</a> <a href="photo2.php">Next Photo</a>
+</body>
+</html>
